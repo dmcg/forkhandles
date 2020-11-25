d12f4bac28f2ba1bf782f799a18fb5a27f3097c8
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-25 22:53:46 +0000

    Document examples

--- values4k/src/test/kotlin/examples.kt
+++ values4k/src/test/kotlin/examples.kt
@@ -11,10 +11,12 @@ import dev.forkhandles.values.minValue
 import dev.forkhandles.values.regex
 
 inline class Money(val value: Int) {
+    override fun toString() = masking(value)
     companion object : IntValueFactory<Money>(::Money, 1.minValue)
 }
 
 class SortCode private constructor(value: String) : StringValue(value) {
+    override fun toString() = masking(value)
     companion object : StringValueFactory<SortCode>(::SortCode, "\\d{6}".regex)
 }
 
@@ -25,14 +27,14 @@ class AccountNumber private constructor(value: String) : Value<String>(value) {
 
 fun main() {
     printOrError { Money.of(1) } // ok
-    printOrError { Money.of(0) } // will blow up
-    printOrError { Money.parse("not money") } // will blow up
+    printOrError { Money.of(0) } // will throw IllegalArgumentException
+    printOrError { Money.parse("not money") } // will return null
 
-    printOrError { SortCode.of("123qwe") } // will blow up
+    printOrError { SortCode.of("123qwe") } // will throw IllegalArgumentException
     printOrError { AccountNumber.of("12345678") } // ok
     printOrError { SortCode.parse("123456") } // ok
-    printOrError { SortCode.of("123qwe") } // will blow up
-    printOrError { AccountNumber.parse("1234567") } // will blow up
+    printOrError { SortCode.of("123qwe") } // will throw IllegalArgumentException
+    printOrError { AccountNumber.parse("1234567") } // will return null
 }
 
 private fun printOrError(fn: () -> Any?) = println(resultFrom(fn).recover { it.message })
